9625650d060b5d571938e4e3918ccf0794eb0c5f
Danh Nguyen <danh.nguyen@axon-networks.com>
2022-06-15 09:45:11 +0700

    Cai Nit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { useEffect, useLayoutEffect } from 'react'
 import './App.css'
 import Footer from './components/Footer/Footer.component'
 import Header from './components/Header/Header.component'
 import Main from './components/Main/Main'
 
 function App() {
-  useLayoutEffect(() => {
-    console.log(`effect layout 1`)
-  }, [])
-  useEffect(() => {
-    console.log(`effect 1`)
-  }, [])
   return (
     <>
       <Header />
