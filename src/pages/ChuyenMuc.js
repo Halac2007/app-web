a1110336c7b8db425f4db32150bb267624b6c2d0
bemai <halac2007@gmail.com>
2022-06-16 16:50:07 +0700

    getdata

--- src/pages/ChuyenMuc.js
+++ src/pages/ChuyenMuc.js
@@ -1,19 +1,29 @@
-import { Box } from '@mui/material'
-import { useEffect } from 'react'
+import { Box, Container, Stack } from '@mui/material'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const ChuyenMuc = () => {
+  const [cates, setCate] = useState([])
   const { name } = useParams()
   useEffect(() => {
-    console.log(`Lay du lieu tuy theo name ${name}`)
+    axios
+      .get(
+        'https://api.escuelajs.co/api/v1/products?fbclid=IwAR2R8gN288AvUTnAGHr6rhU36vrz1xAF_F7bWgOP83U2RmHSyk_D3qmXc_Q'
+      )
+      .then((res) => {
+        setCate(res.data.slice(0, 3).map((t) => t.title))
+        console.log(res.data.slice(0, 3).map((t) => t.title))
+      })
   }, [name])
 
   return (
-    <Box>
-      <Box>Chuyên mục {name}</Box>
-      <Box>Chuyên mục con {name}</Box>
-      <Box>Bài viết {name}</Box>
-    </Box>
+    <Container maxWidth="sm">
+      {cates.map((cate) => (
+        <Box key={cate.id}>{cate.title}</Box>
+      ))}
+      <Stack></Stack>
+    </Container>
   )
 }
 
