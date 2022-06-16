84bf94a5f833d0517bb7329561d5cf19167f28ec
Danh Nguyen <danh.nguyen@axon-networks.com>
2022-06-16 17:00:11 +0700

    Finish api

--- src/pages/ChuyenMuc.js
+++ src/pages/ChuyenMuc.js
@@ -1,10 +1,10 @@
-import { Box, Container, Stack } from '@mui/material'
+import { Box, Stack } from '@mui/material'
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import axios from 'axios'
 
 const ChuyenMuc = () => {
-  const [cates, setCate] = useState([])
+  const [cates, setCates] = useState([])
   const { name } = useParams()
   useEffect(() => {
     axios
@@ -12,18 +12,26 @@ const ChuyenMuc = () => {
         'https://api.escuelajs.co/api/v1/products?fbclid=IwAR2R8gN288AvUTnAGHr6rhU36vrz1xAF_F7bWgOP83U2RmHSyk_D3qmXc_Q'
       )
       .then((res) => {
-        setCate(res.data.slice(0, 3).map((t) => t.title))
-        console.log(res.data.slice(0, 3).map((t) => t.title))
+        const data = res.data.slice(0, 3).map((item) => ({
+          title: item.title,
+          sapo: item.description,
+          image: item.images[0],
+        }))
+        console.log(data)
+        setCates(data)
       })
   }, [name])
 
   return (
-    <Container maxWidth="sm">
-      {cates.map((cate) => (
-        <Box key={cate.id}>{cate.title}</Box>
+    <Box>
+      {cates.map((item) => (
+        <Stack key={item.title}>
+          <Box>{item.title}</Box>
+          <Box>{item.sapo}</Box>
+          <img src={item.image} width="100px" alt="" />
+        </Stack>
       ))}
-      <Stack></Stack>
-    </Container>
+    </Box>
   )
 }
 
